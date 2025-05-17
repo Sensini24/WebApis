47f6491a2d44af34ca792122a645c212bb97f289
Sensini24 <lit.universal_2l@hotmail.com>
2025-05-16 20:19:23 -0500

    Se agrego mas tests como el guardar libro

--- BookApi.Tests/BookEndpointsTests.cs
+++ BookApi.Tests/BookEndpointsTests.cs
@@ -1,6 +1,7 @@
 using System.Net.Http.Json;
 using Xunit;
 using DemoMinimalAPI.Entities;
+using System.Net;
 
 public class BookEndpointsTests : IClassFixture<CustomWebApplicationFactory<Program>>
 {
@@ -21,6 +22,28 @@ public class BookEndpointsTests : IClassFixture<CustomWebApplicationFactory<Prog
         response.EnsureSuccessStatusCode(); // 200
         var json = await response.Content.ReadAsStringAsync();
         Assert.Contains("[]", json); // lista vacía en JSON
+    }
+
+    [Fact]
+    public async Task CreateBook_ReturnTheSameBookCreated()
+    {
+        var newBook = new Book
+        {
+            Title = "Pedro Páramo",
+            Author = "Juan Rulfo",
+            Isbn = "123456789",
+            PublishedDate = new DateTime(1955, 1, 1)
+        };
+        
+        var response = await _client.PostAsJsonAsync("/saveBook", newBook);
+        var createdBook = await response.Content.ReadFromJsonAsync<Book>();
+
+        //response.StatusCode.Should().Be(HttpStatusCode.Created);
+        Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+        Assert.NotNull(createdBook);
+        Assert.True(createdBook.Id > 0);
+        Assert.Equal(newBook.Title, "Perro faldero");
+        Assert.Equal(newBook.Author, createdBook.Author);
     }
     // [Fact]
     // public async Task SaveBook_Then_GetById_WorksCorrectly()
